2f8a8651873c8c5459e3b107e223acc57428fa05
taurusilver7 <prasad20697@gmail.com>
2021-05-06 20:57:42 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
