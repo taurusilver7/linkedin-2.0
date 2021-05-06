b8f9de1d840acf98e23cbf4f2269f4cb35a35368
taurusilver7 <prasad20697@gmail.com>
2021-05-06 22:29:24 +0530

    login styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Nav>
+        <a href="/">
+          <img src="/img/login-logo.svg" alt="login_logo" />
+        </a>
+      </Nav>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Nav = styled.nav`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
