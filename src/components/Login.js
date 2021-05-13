3cf1ae570313111d414b9963f18e8d89f0338986
taurusilver7 <prasad20697@gmail.com>
2021-05-13 23:21:28 +0530

    sign-in button styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -155,4 +155,7 @@ const Google = styled.button`
   border-radius: 28px;
   box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
     inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
 `;
