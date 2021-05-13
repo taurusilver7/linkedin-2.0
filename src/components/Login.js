615a5e17b76a37b809b721ffce87e7d3f06aafb6
taurusilver7 <prasad20697@gmail.com>
2021-05-13 23:22:14 +0530

    hover effects added

--- src/components/Login.js
+++ src/components/Login.js
@@ -160,4 +160,8 @@ const Google = styled.button`
   transition-duration: 167ms;
   font-size: 20px;
   color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+  }
 `;
