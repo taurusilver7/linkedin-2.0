b04420b34a430f70edf352a66e028ad4403127e7
taurusilver7 <prasad20697@gmail.com>
2021-05-15 21:34:20 +0530

    header upto-date

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,40 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-  return <div>
-      Header
-  </div>;
+  return (
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="./img/home-logo.svg" alt="header_logo" />
+          </a>
+        </Logo>
+      </Content>
+    </Container>
+  );
 };
 
 export default Header;
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 0 24px;
+  width: 100vw;
+  z-index: 100;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+`;
+
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0px;
+`;
