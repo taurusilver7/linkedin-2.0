21b8962c7a2505b78b2c15047d6c3c0f20905d9e
taurusilver7 <prasad20697@gmail.com>
2021-05-15 17:10:15 +0530

    header&home added to route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
           <Route exact path="/">
             <Login />
           </Route>
-          <Route path="/sub"></Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
