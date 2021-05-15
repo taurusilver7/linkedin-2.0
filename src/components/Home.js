968324a3b04889c6135fa21e9bb5201f8b341c67
taurusilver7 <prasad20697@gmail.com>
2021-05-15 17:09:30 +0530

    component created

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
